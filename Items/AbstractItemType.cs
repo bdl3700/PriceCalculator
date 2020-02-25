40fe0e06aa976703217a5e087528ca04db30b867
bdl3700 - Laptop <Bdl3700@comcast.net>
2020-02-25 13:47:19 -0700

    Wrote a small console app to test my Price Calculator

---
+++ Items/AbstractItemType.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Items
+{
+    /// <summary>
+    /// The basic outline of everything that an ItemType needs to have in order to work in
+    /// the PriceCalculator.
+    /// </summary>
+    public abstract class AbstractItemType
+    {
+        /// <summary>
+        /// A decimal representation of the amount of margin desired. Represented as 1-(percent markup).
+        /// 
+        /// EX: a 15% markup would have a margin of .85
+        ///     a 45% markup would have a margin of .55
+        ///     etc.
+        /// </summary>
+        protected double Margin { get; set;}
+
+        /// <summary>
+        /// The primary method that will be used by the Controller/GUI to calculate the
+        /// retail price of the item in question.
+        /// </summary>
+        /// <param name="cost">The cost of the item</param>
+        /// <returns>The retail price of the Item</returns>
+        public abstract double Calculate(double cost);
+        
+        /// <summary>
+        /// Follows simple rules to cleanup a price and make it more presentable
+        /// </summary>
+        /// <returns></returns>
+        protected double CleanupPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Used to round the price of the given price to the specified threshold
+        /// </summary>
+        /// <param name="price">The unrounded price of the item</param>
+        /// <param name="threshold">The number of dollars to which to round the price</param>
+        /// <returns>A rounded price</returns>
+        public abstract double Round(double price, double threshold);
+
+        /// <summary>
+        /// Used to set the cents of the price prior to reporting the final price.
+        /// </summary>
+        /// <param name="price">The rounded price of the item</param>
+        /// <returns>The input price with the appropriate amount of cents</returns>
+        public double PrepDecimal(double price)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PriceCalculator Test/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Items;
+
+namespace PriceCalculator_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> supportedItemTypes = new HashSet<string>() {"firearm", "ammo", "accessory" };
+            Console.WriteLine("Please enter an item type: ");
+            string type = Console.ReadLine().ToLower();
+            AbstractItemType item = null;
+
+            if (!supportedItemTypes.Contains(type))
+            {
+                Console.WriteLine($"Item Type not supported, please select an option from: {supportedItemTypes.ToString()}");
+                Main(args);
+            }
+            
+            switch (type)
+            {
+                case "firearm":
+                    item = new Firearm();
+                    break;
+                case "accessory":
+                    item = new Accessory();
+                    break;
+                case "ammo":
+                    item = new Accessory();
+                    break;
+            }
+
+            Console.WriteLine("Enter the item's cost: ");
+            if (!double.TryParse(Console.ReadLine(), out double cost))
+            {
+                Console.WriteLine("That wasn't a valid number, let's try that again.");
+                Main(args);
+            }
+
+            Console.WriteLine($"That {type} would cost ${item.Calculate(cost)}");
+        }
+    }
+}
